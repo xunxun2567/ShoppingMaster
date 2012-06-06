0c2dffcf5e4f23be8fb29d5f3098c5dc023faa8e
konglingkai <konglingkai@egibbon.com>
2012-06-06 15:03:33 +0800

    Settings Controller

---
+++ SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  ShoppingMaster
+//
+//  Created by Lingkai Kong on 12-6-6.
+//  Copyright (c) 2012年 Egibbon Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
